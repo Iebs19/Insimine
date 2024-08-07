bcbde23841447e8cadfa42a9ffda19153491ace1
Iebs19 <krishnamohanpoddar@iebrain.com>
2024-08-07 11:07:08 +0530

    Hero Section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -52,11 +52,13 @@ function App() {
         <div className='flex flex-col justify-center'>
           <GradualSpacing className='font-nas text-center font-bold pointer-events-none bg-gradient-to-b from-green to-lightBlue bg-clip-text text-9xl font-semibold leading-none text-transparent dark:from-green dark:to-lightBlue' text='InsiMine' duration={5} />
           <GradualSpacing className='font-nas text-4xl text-left' text='Mining Insights for Data Driven Success.' />
-          <GradualSpacing className='font-nas text-left' text='InsiMine is a trusted AI  ' />
+          {/* <GradualSpacing className='font-nas text-left' text='InsiMine is a trusted AI  ' />
           <GradualSpacing className='font-nas text-left' text='and analytics-based solutions provider empowering' />
           <GradualSpacing className='font-nas text-left' text=' pharmaceutical and healthcare industries to make informed,' />
-          <GradualSpacing className='font-nas text-left' text=' data-driven decisions.' />
-          <div className='flex flex-row justify-center pt-4 gap-8'>
+          <GradualSpacing className='font-nas text-left' text=' data-driven decisions.' /> */}
+          <BlurFade delay={1} inView >
+            <p className='font-nas text-center space-x-1 p-8'>InsiMine is a trusted AI and analytics-based solutions provider empowering pharmaceutical and healthcare industries to make informed, data-driven decisions.</p>
+            <div className='flex flex-row justify-center gap-8'>
             <ShimmerButton className="shadow-2xl" background='darkBlue' borderRadius='4px'>
               <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
                 Learn More
@@ -65,6 +67,8 @@ function App() {
             <PulsatingButton className='' pulseColor='green'>Book a call</PulsatingButton>
 
           </div>
+          </BlurFade>
+          
         </div>
       </Layout>
     </>
