42e88ee7ad44a741249223d4a2f8933cbe3b5477
Iebs19 <krishnamohanpoddar@iebrain.com>
2024-08-07 10:58:50 +0530

    Hero Section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,39 +6,66 @@ import Particles from "@/components/magicui/particles";
 import Layout from './Layout';
 import logo from "../src/assets/favicon_insimine.png";
 import GradualSpacing from "@/components/magicui/gradual-spacing";
-import {motion} from 'framer-motion';
-import { BorderBeam } from "@/components/magicui/border-beam";
+import IconCloud from "@/components/magicui/icon-cloud";
+import BlurFade from "@/components/magicui/blur-fade";
+import ShimmerButton from "@/components/magicui/shimmer-button";
+import PulsatingButton from "@/components/ui/pulsating-button";
 
 function App() {
   const [count, setCount] = useState(0);
+  const slugs = [
+    "typescript",
+    "javascript",
+    "dart",
+    "java",
+    "react",
+    "flutter",
+    "android",
+    "html5",
+    "css3",
+    "nodedotjs",
+    "express",
+    "nextdotjs",
+    "prisma",
+    "amazonaws",
+    "postgresql",
+    "firebase",
+    "nginx",
+    "vercel",
+    "testinglibrary",
+    "jest",
+    "cypress",
+    "docker",
+    "git",
+    "jira",
+    "github",
+    "gitlab",
+    "visualstudiocode",
+    "androidstudio",
+    "sonarqube",
+    "figma",
+  ];
 
   return (
     <>
       <Layout>
-        <div className=''>
-        <div className='relative flex flex-col justify-center text-left'>
-            <div className='font-nas text-center font-bold pointer-events-none bg-gradient-to-b from-green to-lightBlue bg-clip-text text-9xl font-semibold leading-none text-transparent dark:from-green dark:to-lightBlue'>InsiMine</div>
-            <GradualSpacing className='font-nas text-4xl text-left' text='Mining Insights for Data Driven Success.' />
-        </div>
-          {/* <BorderBeam size={250} duration={12} delay={9} colorFrom='darkBlue' colorTo='green' borderWidth={2.5} /> */}
-        </div>
+        <div className='flex flex-col justify-center'>
+          <GradualSpacing className='font-nas text-center font-bold pointer-events-none bg-gradient-to-b from-green to-lightBlue bg-clip-text text-9xl font-semibold leading-none text-transparent dark:from-green dark:to-lightBlue' text='InsiMine' duration={5} />
+          <GradualSpacing className='font-nas text-4xl text-left' text='Mining Insights for Data Driven Success.' />
+          <GradualSpacing className='font-nas text-left' text='InsiMine is a trusted AI  ' />
+          <GradualSpacing className='font-nas text-left' text='and analytics-based solutions provider empowering' />
+          <GradualSpacing className='font-nas text-left' text=' pharmaceutical and healthcare industries to make informed,' />
+          <GradualSpacing className='font-nas text-left' text=' data-driven decisions.' />
+          <div className='flex flex-row justify-center pt-4 gap-8'>
+            <ShimmerButton className="shadow-2xl" background='darkBlue' borderRadius='4px'>
+              <span className="whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 lg:text-lg">
+                Learn More
+              </span>
+            </ShimmerButton>
+            <PulsatingButton className='' pulseColor='green'>Book a call</PulsatingButton>
 
-          {/* <div className='flex-shrink-0'> */}
-            {/* <img src={logo} alt="Insimine-logo" /> */}
-            {/* <motion.img
-              // id={classes["floater"]}
-              initial={{ y: -10 }}
-              animate={{ y: 10 }}
-              transition={{
-                type: "smooth",
-                repeatType: "mirror",
-                duration: 2,
-                repeat: Infinity,
-              }}
-              src={logo}
-              alt="floater"
-            /> */}
-          {/* </div> */}
+          </div>
+        </div>
       </Layout>
     </>
   );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -77,11 +77,37 @@ module.exports = {
             "offset-distance": "100%",
           },
         },
+        "spin-around": {
+          "0%": {
+            transform: "translateZ(0) rotate(0)",
+          },
+          "15%, 35%": {
+            transform: "translateZ(0) rotate(90deg)",
+          },
+          "65%, 85%": {
+            transform: "translateZ(0) rotate(270deg)",
+          },
+          "100%": {
+            transform: "translateZ(0) rotate(360deg)",
+          },
+        },
+        slide: {
+          to: {
+            transform: "translate(calc(100cqw - 100%), 0)",
+          },
+        },
+        pulse: {
+          "0%, 100%": { boxShadow: "0 0 0 0 var(--pulse-color)" },
+          "50%": { boxShadow: "0 0 0 8px var(--pulse-color)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         "border-beam": "border-beam calc(var(--duration)*1s) infinite linear",
+        "spin-around": "spin-around calc(var(--speed) * 2) infinite linear",
+        slide: "slide var(--speed) ease-in-out infinite alternate",
+        pulse: "pulse var(--duration) ease-out infinite",
       },
     },
   },
