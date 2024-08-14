678e9362edd43c722bf35b1bee653a3e06e2a534
Iebs19 <krishnamohanpoddar@iebrain.com>
2024-08-14 13:14:47 +0530

    Apnay saath ab feel karoon aadha bhi

--- backend/insimine/urls.py
+++ backend/insimine/urls.py
@@ -16,14 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from frontend import views as frontend_views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('app.urls')),
-    path('', frontend_views.index, name='index'),
 ]
 
 
